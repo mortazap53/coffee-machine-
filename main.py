0a4a00a6823d01524c1128f043be2aa0836b7088
R1stAMOR <mortazap53@gmail.com>
2026-01-16 22:52:01 -0800

    update the changes

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+# importing:
+from core.menu import MENU
+
+
+
+
+# functions:
+def show_menu(data):
+    for choice in data:
+        price = data[choice]["cost"]
+        print(f"{choice.title()}" + " ==> " + f"${price}")
+
+
+
+
 print("Welcome to the Coffee Machine App")
 print("Here is the menu:\n")
 show_menu(MENU)
