8f656fb1daa0a274d5f530f12e77a3ebd6a1a962
R1stAMOR <mortazap53@gmail.com>
2026-01-17 00:03:30 -0800

    update the changes

---
+++ core/Resources.py
@@ -0,0 +1,5 @@
+resources = {
+    "water": 500,
+    "coffee": 500,
+    "milk": 300,
+}
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # importing:
-from core.menu import MENU
+from core.Menu import MENU
+from core.Payment import collecting_money
+from core.Engine import checking_resources
+from core.Resources import resources
 
 
 
@@ -16,7 +19,9 @@ def show_menu(data):
 print("Welcome to the Coffee Machine App")
 print("Here is the menu:\n")
 show_menu(MENU)
-take_order = input("What would you like?")
+take_order = input("What would you like?\n==> ").lower()
+print(checking_resources(take_order, resources))
+# collecting_money()
 
 
 
