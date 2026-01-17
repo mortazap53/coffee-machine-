b38e8329fcc2e393f1b06468f7e5a94854cf0cfa
Mortaza <mortazap53@gmail.com>
2026-01-16 23:26:38 -0800

    update the changes

--- core/payment.py
+++ core/payment.py
@@ -1,9 +1,9 @@
-def collecting_price():
-    collected_price = (0.25*quarters) + (0.10*dimes) + (0.05*nickles) + (0.01*pennies)
+def collecting_money():
+    print("pay the price, please!:")
+    pennies = float(input("How many pennies?"))
+    nickles = float(input("How many nickles?"))
+    dimes = float(input("How many dimes?"))
+    quarters = float(input("How many quarters?"))
+    collected_price = round((0.25*quarters) + (0.10*dimes) + (0.05*nickles) + (0.01*pennies), 2)
     return collected_price
 
-print("pay the price, please!:")
-pennies = float(input("How many pennies?"))
-nickles = float(input("How many nickles?"))
-dimes = float(input("How many dimes?"))
-quarters = float(input("How many quarters?"))
